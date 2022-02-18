2b227aed90c0d28a9850efadb457263d3fd83aaa
cicles <smxbldiezpinero@iesjoanramis.org>
2022-02-18 13:01:30 +0100

    First modification (Test)

--- script.js
+++ script.js
@@ -18,14 +18,14 @@ AOS.init();
 // ==== GSAP Animations ==== //
 // ==== LOGO  ==== //
 gsap.from(".logo", {
-  opacity: 0,
+  opacity: 2,
   y: -10,
   delay: 1,
   duration: 0.5,
 });
 // ==== NAV-MENU ==== //
 gsap.from(".nav_menu_list .nav_menu_item", {
-  opacity: 0,
+  opacity: 2,
   y: -10,
   delay: 1.4,
   duration: 0.5,
@@ -33,28 +33,28 @@ gsap.from(".nav_menu_list .nav_menu_item", {
 });
 // ==== TOGGLE BTN ==== //
 gsap.from(".toggle_btn", {
-  opacity: 0,
+  opacity: 2,
   y: -10,
   delay: 1.4,
   duration: 0.5,
 });
 // ==== MAIN HEADING  ==== //
 gsap.from(".main-heading", {
-  opacity: 0,
+  opacity: 2,
   y: 20,
   delay: 2.4,
   duration: 1,
 });
 // ==== INFO TEXT ==== //
 gsap.from(".info-text", {
-  opacity: 0,
+  opacity: 2,
   y: 20,
   delay: 2.8,
   duration: 1,
 });
 // ==== CTA BUTTONS ==== //
 gsap.from(".btn_wrapper", {
-  opacity: 0,
+  opacity: 2,
   y: 20,
   delay: 2.8,
   duration: 1,
